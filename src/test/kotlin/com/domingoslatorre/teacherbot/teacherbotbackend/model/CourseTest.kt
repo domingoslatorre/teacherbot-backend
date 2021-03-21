ead6060fed338f5598e90c1343cd4c74671f7432
Domingos Latorre <domingos.latorre@gmail.com>
2021-03-21 17:30:00 -0300

    Create course model tests

---
+++ src/test/kotlin/com/domingoslatorre/teacherbot/teacherbotbackend/model/CourseTest.kt
@@ -0,0 +1,78 @@
+package com.domingoslatorre.teacherbot.teacherbotbackend.model
+
+import com.domingoslatorre.teacherbot.teacherbotbackend.course.model.ModuleAlreadyExists
+import com.domingoslatorre.teacherbot.teacherbotbackend.factory.CourseFactory
+import com.domingoslatorre.teacherbot.teacherbotbackend.factory.ModuleFactory
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.inspectors.forAtLeastOne
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
+
+class CourseTest : FunSpec({
+    context("Create a course") {
+        val course = CourseFactory.course()
+
+        test("should has a empty set of modules") {
+            course.modules.shouldBeEmpty()
+            course.modules.shouldBeInstanceOf<MutableSet<Module>>()
+        }
+    }
+
+    context("Create a course and add modules") {
+        val course = CourseFactory.course()
+
+        val module1 = ModuleFactory.module(title = "Module 1")
+        course.addModule(module1.title, module1.objective, module1.position)
+
+        val module2 = ModuleFactory.module(title = "Module 2")
+        course.addModule(module2.title, module2.objective, module2.position)
+
+        test("should have two modules") {
+            course.modules shouldHaveSize 2
+        }
+
+        test("should have module1 and module2 data") {
+            course.modules.forAtLeastOne {
+                it.title shouldBe module1.title
+                it.objective shouldBe module1.objective
+                it.position shouldBe module1.position
+            }
+
+            course.modules.forAtLeastOne {
+                it.title shouldBe module2.title
+                it.objective shouldBe module2.objective
+                it.position shouldBe module2.position
+            }
+        }
+    }
+
+    context("Create a course and add modules with same title") {
+        val course = CourseFactory.course()
+        val title = "Module 1"
+
+        val module1 = ModuleFactory.module(title = title)
+        course.addModule(module1.title, module1.objective, module1.position)
+
+        val module2 = ModuleFactory.module(title = title)
+        val result = course.addModule(module2.title, module2.objective, module2.position)
+
+        test("should have one modules") {
+            course.modules shouldHaveSize 1
+        }
+
+        test("should return a result of failure") {
+            result.isFailure.shouldBeTrue()
+        }
+
+        test("should return a ModuleAlreadyExists") {
+            result.exceptionOrNull()!!.shouldBeInstanceOf<ModuleAlreadyExists>()
+        }
+
+        test("should return a message") {
+            result.exceptionOrNull()!!.message shouldBe "Module with title $title already exists in course ${course.id}"
+        }
+    }
+})
