7bda8415e09fc668213260279a8262cf7c54ed67
Domingos Latorre <domingos.latorre@gmail.com>
2021-03-21 20:30:39 -0300

    Add integration tests for module create feature

---
+++ src/test/kotlin/com/domingoslatorre/teacherbot/teacherbotbackend/integration/course/module/ModuleIntegrationTest.kt
@@ -0,0 +1,36 @@
+package com.domingoslatorre.teacherbot.teacherbotbackend.integration.course.module
+
+import com.domingoslatorre.teacherbot.teacherbotbackend.common.ProblemDetail
+import com.domingoslatorre.teacherbot.teacherbotbackend.course.api.requests.ModuleReq
+import com.domingoslatorre.teacherbot.teacherbotbackend.course.service.dto.ModuleDto
+import com.domingoslatorre.teacherbot.teacherbotbackend.integration.course.CourseIntegrationTest
+import com.domingoslatorre.teacherbot.teacherbotbackend.integration.course.moduleListResParam
+import com.domingoslatorre.teacherbot.teacherbotbackend.integration.course.moduleResParam
+import com.domingoslatorre.teacherbot.teacherbotbackend.integration.course.problemDetailParam
+import java.util.UUID
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.boot.test.web.client.postForEntity
+import org.springframework.http.ResponseEntity
+
+abstract class ModuleIntegrationTest : CourseIntegrationTest() {
+    abstract override val restTemplate: TestRestTemplate
+
+    private fun modulesUrl(courseId: UUID) =
+        restTemplate.restTemplate.uriTemplateHandler.expand("/courses/{courseId}/modules", courseId).toString()
+
+    private fun moduleUrl(courseId: UUID, moduleId: UUID) = restTemplate.restTemplate
+        .uriTemplateHandler.expand("/courses/{courseId}/modules/{moduleId}", courseId, moduleId).toString()
+
+    protected fun getModules(courseId: UUID): ResponseEntity<List<ModuleDto>> =
+        restTemplate.getForEntity(modulesUrl(courseId), moduleListResParam)
+
+    protected fun getModuleById(courseId: UUID, moduleId: UUID): ResponseEntity<ModuleDto> =
+        restTemplate.getForEntity(moduleUrl(courseId, moduleId), moduleResParam, courseId, moduleId)
+
+    protected fun postModule(courseId: UUID, moduleReq: ModuleReq): ResponseEntity<ModuleDto> =
+        restTemplate.postForEntity(modulesUrl(courseId), moduleReq, moduleResParam)
+
+    protected fun postModuleProblemDetail(courseId: UUID, moduleReq: ModuleReq): ResponseEntity<ProblemDetail> =
+        restTemplate.postForEntity(modulesUrl(courseId), moduleReq, problemDetailParam)
+}
