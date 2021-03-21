62ca8112cb6068ae94e97ef85edf1cc0fe87a213
Domingos Latorre <domingos.latorre@gmail.com>
2021-03-21 20:27:59 -0300

    Refactor api exception handler

--- src/main/kotlin/com/domingoslatorre/teacherbot/teacherbotbackend/common/exceptions.kt
+++ src/main/kotlin/com/domingoslatorre/teacherbot/teacherbotbackend/common/exceptions.kt
@@ -1,5 +1,9 @@
 package com.domingoslatorre.teacherbot.teacherbotbackend.common
 
+import com.domingoslatorre.teacherbot.teacherbotbackend.course.model.ModuleAlreadyExistsException
+import com.domingoslatorre.teacherbot.teacherbotbackend.course.model.ModuleNotFoundException
+import com.domingoslatorre.teacherbot.teacherbotbackend.course.service.CourseAlreadyExistsException
+import com.domingoslatorre.teacherbot.teacherbotbackend.course.service.CourseNotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.MethodArgumentNotValidException
@@ -7,21 +11,36 @@ import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 data class Violation(val name: String, val reason: String)
-data class ProblemDetail(val title: String, val violations: List<Violation>)
-
-class AlreadyExistsException : RuntimeException()
-class NotFoundException : RuntimeException()
+data class ProblemDetail(val title: String, val violations: List<Violation> = listOf())
 
 @Suppress("unused")
 @ControllerAdvice
 class GlobalExceptionHandler {
-    @ExceptionHandler(value = [AlreadyExistsException::class])
-    fun handleAlreadyExistsException(exception: AlreadyExistsException) =
-        ResponseEntity(ProblemDetail("Resource already exists", listOf()), HttpStatus.CONFLICT)
 
-    @ExceptionHandler(value = [NotFoundException::class])
-    fun handleNotFoundError(exception: NotFoundException) =
-        ResponseEntity(ProblemDetail("Resource not found", listOf()), HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = [CourseAlreadyExistsException::class])
+    fun handleCourseAlreadyExistsException(exception: CourseAlreadyExistsException) =
+        ResponseEntity(
+            ProblemDetail("Course already exists with name ${exception.name} and acronym ${exception.acronym}"),
+            HttpStatus.CONFLICT
+        )
+
+    @ExceptionHandler(value = [CourseNotFoundException::class])
+    fun handleCourseNotFoundException(exception: CourseNotFoundException) =
+        ResponseEntity(ProblemDetail("Course not found with id ${exception.id}"), HttpStatus.NOT_FOUND)
+
+    @ExceptionHandler(value = [ModuleAlreadyExistsException::class])
+    fun handleModuleAlreadyExistsException(exception: ModuleAlreadyExistsException) =
+        ResponseEntity(
+            ProblemDetail("Module already exists with title ${exception.title} course id ${exception.courseId}"),
+            HttpStatus.CONFLICT
+        )
+
+    @ExceptionHandler(value = [ModuleNotFoundException::class])
+    fun handleModuleNotFoundException(exception: ModuleNotFoundException) =
+        ResponseEntity(
+            ProblemDetail("Module not found with id ${exception.moduleId} and course id ${exception.courseId}"),
+            HttpStatus.NOT_FOUND
+        )
 
     @ExceptionHandler(value = [MethodArgumentNotValidException::class])
     fun handleMethodArgumentNotValidException(exception: MethodArgumentNotValidException) =
