ba02f0e6c44c74574de7eb0b67e17f04f19c0593
Domingos Latorre <domingos.latorre@gmail.com>
2021-03-21 17:26:10 -0300

    Create addModule logic and change modules collection to mutable set

--- src/main/kotlin/com/domingoslatorre/teacherbot/teacherbotbackend/course/model/Course.kt
+++ src/main/kotlin/com/domingoslatorre/teacherbot/teacherbotbackend/course/model/Course.kt
@@ -1,11 +1,14 @@
 package com.domingoslatorre.teacherbot.teacherbotbackend.course.model
 
+import java.lang.RuntimeException
 import java.util.UUID
 import javax.persistence.CascadeType
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.OneToMany
+
+data class ModuleAlreadyExists(override val message: String) : RuntimeException(message)
 
 @Entity
 class Course(
@@ -17,7 +20,7 @@ class Course(
 
     @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true)
     @JoinColumn(name = "course_id")
-    val modules: MutableList<Module> = mutableListOf(),
+    val modules: MutableSet<Module> = mutableSetOf(),
 ) {
     fun update(name: String, description: String, acronym: String) = Course(
         id = id,
@@ -26,4 +29,10 @@ class Course(
         description = description,
         modules = modules
     )
+
+    fun addModule(title: String, objective: String, position: Int): Result<Module> =
+        Module(title = title, objective = objective, position = position).let {
+            if (modules.add(it)) Result.success(it)
+            else Result.failure(ModuleAlreadyExists("Module with title $title already exists in course $id"))
+        }
 }
