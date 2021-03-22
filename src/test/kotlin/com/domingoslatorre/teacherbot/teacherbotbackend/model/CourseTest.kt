5079076699d179ca044dc70fac214800ec0f4ad3
Domingos Latorre <domingos.latorre@gmail.com>
2021-03-22 08:36:25 -0300

    Create test for update module with same title

--- src/test/kotlin/com/domingoslatorre/teacherbot/teacherbotbackend/model/CourseTest.kt
+++ src/test/kotlin/com/domingoslatorre/teacherbot/teacherbotbackend/model/CourseTest.kt
@@ -133,4 +133,68 @@ class CourseTest : FunSpec({
             }
         }
     }
+
+    context("Create a course, add modules and update with same title") {
+        val course = CourseFactory.course()
+
+        val module1 = ModuleFactory.module(title = "Module 1")
+        course.addModule(module1.title, module1.objective, module1.position)
+
+        val module2 = ModuleFactory.module(title = "Module 2")
+        val module2Add = course.addModule(module2.title, module2.objective, module2.position).getOrNull()!!
+
+        test("should have two modules before update") {
+            course.modules shouldHaveSize 2
+        }
+
+        test("should have module1 and module2 data") {
+            course.modules.forAtLeastOne {
+                it.title shouldBe module1.title
+                it.objective shouldBe module1.objective
+                it.position shouldBe module1.position
+            }
+
+            course.modules.forAtLeastOne {
+                it.title shouldBe module2.title
+                it.objective shouldBe module2.objective
+                it.position shouldBe module2.position
+            }
+        }
+
+        val updatedModule = ModuleFactory.module(
+            id = module2Add.id,
+            title = module1.title,
+            objective = "Module 3 objective",
+            position = 3
+        )
+
+        val result =
+            course.updateModule(updatedModule.id, updatedModule.title, updatedModule.objective, updatedModule.position)
+
+        test("should return a result of failure") {
+            result.isFailure.shouldBeTrue()
+        }
+
+        test("should return a ModuleAlreadyExistsException") {
+            result.exceptionOrNull()!!.shouldBeInstanceOf<ModuleAlreadyExistsException>()
+        }
+
+        test("should have two modules after update") {
+            course.modules shouldHaveSize 2
+        }
+
+        test("should not update module1 and module2 data") {
+            course.modules.forAtLeastOne {
+                it.title shouldBe module1.title
+                it.objective shouldBe module1.objective
+                it.position shouldBe module1.position
+            }
+
+            course.modules.forAtLeastOne {
+                it.title shouldBe module2.title
+                it.objective shouldBe module2.objective
+                it.position shouldBe module2.position
+            }
+        }
+    }
 })
