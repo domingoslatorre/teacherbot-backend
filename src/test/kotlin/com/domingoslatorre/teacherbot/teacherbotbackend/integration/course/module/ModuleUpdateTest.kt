8667d551410afbe7b1f185acd69ce8d135ee8218
Domingos Latorre <domingos.latorre@gmail.com>
2021-03-22 09:14:49 -0300

    Create integration test for module update with same title

--- src/test/kotlin/com/domingoslatorre/teacherbot/teacherbotbackend/integration/course/module/ModuleUpdateTest.kt
+++ src/test/kotlin/com/domingoslatorre/teacherbot/teacherbotbackend/integration/course/module/ModuleUpdateTest.kt
@@ -2,13 +2,14 @@ package com.domingoslatorre.teacherbot.teacherbotbackend.integration.course.modu
 
 import com.domingoslatorre.teacherbot.teacherbotbackend.factory.CourseReqFactory
 import com.domingoslatorre.teacherbot.teacherbotbackend.factory.ModuleReqFactory
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.http.HttpStatus
 
-class ModuleUpdateTest (@Autowired override val restTemplate: TestRestTemplate) : ModuleIntegrationTest() {
+class ModuleUpdateTest(@Autowired override val restTemplate: TestRestTemplate) : ModuleIntegrationTest() {
 
     @Test
     fun `PUT module`() {
@@ -36,6 +37,31 @@ class ModuleUpdateTest (@Autowired override val restTemplate: TestRestTemplate) 
                 objective shouldBe moduleReqEdit.objective
                 position shouldBe moduleReqEdit.position
             }
+        }
+    }
+
+    @Test
+    fun `PUT module - same title`() {
+        val courseRes = postCourse(CourseReqFactory.courseReq()).body!!
+
+        val moduleReq1 = ModuleReqFactory.moduleReq(title = "Module 1")
+        postModule(courseRes.id, moduleReq1).body!!
+
+        val moduleReq2 = ModuleReqFactory.moduleReq(title = "Module 2")
+        val moduleRes2 = postModule(courseRes.id, moduleReq2).body!!
+
+        val moduleReqEdit = moduleReq2.copy(title = moduleReq1.title!!)
+
+        putModuleProblemDetail(courseRes.id, moduleRes2.id, moduleReqEdit).apply {
+            statusCode shouldBe HttpStatus.CONFLICT
+            body!!.title shouldBe "Module already exists with title ${moduleReqEdit.title} course id ${courseRes.id}"
+        }
+
+        getModuleById(courseRes.id, moduleRes2.id).body!!.apply {
+            id.shouldNotBeNull()
+            title shouldBe moduleReq2.title
+            objective shouldBe moduleReq2.objective
+            position shouldBe moduleReq2.position
         }
     }
 }
